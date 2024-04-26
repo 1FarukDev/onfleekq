84d153450568733e083218e13f1f5a12872c75ad
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-04-26 17:11:27 +0100

    feat: Added the graph section

--- src/components/HomeDashboard.js
+++ src/components/HomeDashboard.js
@@ -3,6 +3,7 @@ import Navbar from "../layouts/NavBar";
 import AnalyticCard from "./analyticCard";
 import "../styles/home_dashboard_style.css";
 import TransactionCard from "./transactionCard";
+import TimeCard from "./timeCard";
 
 const HomeDashboard = () => {
   const analyticCardProps = [
@@ -25,25 +26,33 @@ const HomeDashboard = () => {
       number: "10,000",
     },
   ];
-   
+
   return (
     <main>
       <Navbar />
       <main className="dashboard_body">
-        <div className="analytic_card">
-          {analyticCardProps.map((prop, index) => {
-            const isLast = index === analyticCardProps.length - 1;
-            return (
-              <AnalyticCard
-                number={prop.number}
-                text={prop.text}
-                graph={prop.graph}
-                key={index}
-                isLast={isLast}
-              />
-            );
-          })}
+        <div className="analytic_card_body">
+          <div className="analytic_card">
+            {analyticCardProps.map((prop, index) => {
+              const isLast = index === analyticCardProps.length - 1;
+              return (
+                <AnalyticCard
+                  number={prop.number}
+                  text={prop.text}
+                  graph={prop.graph}
+                  key={index}
+                  isLast={isLast}
+                />
+              );
+            })}
+          </div>
+          <div>
+            <div>
+              <TimeCard />
+            </div>
+          </div>
         </div>
+
         <div className="transaction_card">
           <TransactionCard />
         </div>
